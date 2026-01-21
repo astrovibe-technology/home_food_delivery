4f32a35d34afce74bd5c9fe02c959b575e1a2356
Preetha <palanisamypreetha500@gmail.com>
2026-01-21 15:11:43 -0800

    models.py

---
+++ app/models/incentive.py
@@ -0,0 +1,8 @@
+class Incentive(Base):
+    __tablename__ = "incentives"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Integer, nullable=False)
+    reason = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
