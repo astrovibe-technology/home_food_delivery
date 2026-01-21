dc1bfbe8aa35ebee8c885364d67014ab992dc278
Preethapalanisamy <118963839+Preethapalanisamy@users.noreply.github.com>
2026-01-21 01:43:15 -0800

    Delete app/models/faq.py

--- app/models/faq.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database.db import Base
-
-class FAQ(Base):
-    __tablename__ = "faqs"
-
-    id = Column(Integer, primary_key=True)
-    question = Column(String, nullable=False)
-    answer = Column(String, nullable=False)
