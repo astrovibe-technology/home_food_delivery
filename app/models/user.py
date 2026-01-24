317b40d5d6cc47f0f799bbd350b8062ee8487846
Preetha <palanisamypreetha500@gmail.com>
2026-01-23 18:48:29 -0800

    user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from app.database.db import Base  
+
 class User(Base):
     __tablename__ = "users"
 
