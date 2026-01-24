f7bc23869adbc8a106621d75fc23c048da6bcf82
Preetha <palanisamypreetha500@gmail.com>
2026-01-24 12:29:29 -0800

    create_tables.py

--- app/create_tables.py
+++ app/create_tables.py
@@ -1,5 +1,20 @@
 from database.db import engine, Base
 from models.user import User
+from models.address import Address
+from models.cart import Cart
+from models.cartitem import CartItem
+from models.menu import Menu
+from models.restaurant import Restaurant
+from models.cookingstatus import CookingStatus
+from models.faq import FAQ
+from models.incentive import Incentive
+from models.order import Order
+from models.orderitem import OrderItem
+from models.pooling_order import PoolingOrder
+from models.referral import Referral
+from models.user_setting import UserSettings
+from models.wallet_transaction import WalletTransaction
+from models.wallet import Wallet
 
-print(Base.metadata.tables.keys())  # 👈 add this
 Base.metadata.create_all(bind=engine)
+print("All tables created")
