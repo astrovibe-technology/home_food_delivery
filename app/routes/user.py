ceb778e33c8cd2ad19f331f026b812eb44de6faf
Preetha <palanisamypreetha500@gmail.com>
2026-01-26 11:43:52 -0800

    user.py

--- app/routes/user.py
+++ app/routes/user.py
@@ -2,29 +2,62 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from database.db import get_db
+from database.db import SessionLocal
 from models.user import User
-from database.login import LoginRequest, LoginResponse, MessageResponse
-from lib_tu.utils import verify_password, create_access_token
+from database.login import MessageResponse,UserRegisterSchema
+from lib_tu.utils import verify_password, create_access_token, get_password_hash
+
+
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.post("/login", response_model=LoginResponse)
-def login(payload: LoginRequest, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == payload.email).first()
+
+@router.post("/register")
+def register_user(
+    payload: UserRegisterSchema,
+    db: Session = Depends(get_db)
+):
+    email = payload.email.lower()
+
+    existing = db.query(User).filter(User.email == email).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    user = User(
+        name=payload.name,
+        email=email,
+        password=get_password_hash(payload.password),
+        is_active=True
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return {"message": "User registered successfully"}
+
+
+@router.post("/login")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
 
     if not user:
-        raise HTTPException(status_code=400, detail="Invalid email or password")
+        raise HTTPException(status_code=400, detail="User not found")
 
-    if not verify_password(payload.password, user.password):
-        raise HTTPException(status_code=400, detail="Invalid email or password")
-
-    if not user.is_active:
-        raise HTTPException(status_code=403, detail="User inactive")
+    if not verify_password(password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid password")
 
     token = create_access_token({"user_id": user.id})
 
     return {
+        "message": "Login successful",
         "access_token": token,
         "token_type": "bearer"
     }
