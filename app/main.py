cd88d333b21bde3629f2d0267f67d6b41d3f94a5
Preetha <palanisamypreetha500@gmail.com>
2026-01-27 14:30:14 -0800

    main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import os
+import time
 from fastapi import FastAPI
 from routes.user import router as auth_router
+from routes.home import router as home_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(home_router)
