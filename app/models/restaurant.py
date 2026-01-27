4d7e73fded307a2d81dcf1cd87f31413dadb2073
Preetha <palanisamypreetha500@gmail.com>
2026-01-27 14:22:34 -0800

    restaurant.py

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -1,12 +1,20 @@
-from sqlalchemy import Column, Integer, String, Boolean , DateTime 
+from sqlalchemy import Column, Integer, String, Boolean, Date, Time, DateTime
 from datetime import datetime
 from database.db import Base
 
 class Restaurant(Base):
     __tablename__ = "restaurants"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     address = Column(String)
     is_active = Column(Boolean, default=True)
+
+    workplace_id = Column(Integer)
+
+    start_date = Column(Date)
+    start_time = Column(Time)
+    end_time = Column(Time)
+
     created_at = Column(DateTime, default=datetime.utcnow)
+
