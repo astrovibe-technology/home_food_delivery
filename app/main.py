469105abef196b7e540cf0ac16cecd7cc1ca37f0
Preetha <palanisamypreetha500@gmail.com>
2026-01-31 09:54:26 -0800

    main.py

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from models.address import Address
 from models.order import Order       
 from models.orderitem import OrderItem 
 from models.promocode import PromoCode
+from models.shop import Shop
 
 # Routers
 from routes.user import router as auth_router
@@ -19,6 +20,7 @@ from routes.home import router as home_router
 from routes.cart import router as cart_router
 from routes.menu import router as menu_router
 from routes.promocode import router as promocode_router
+from routes.shop import router as shop_router
 
 
 from database.db import engine, Base
@@ -32,3 +34,4 @@ app.include_router(home_router)
 app.include_router(cart_router)
 app.include_router(menu_router)
 app.include_router(promocode_router)
+app.include_router(shop_router)
