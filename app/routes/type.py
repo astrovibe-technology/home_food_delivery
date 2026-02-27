c3c28f3180638c8f5aabd217e8840eb94913e212
Preetha <palanisamypreetha500@gmail.com>
2026-02-27 12:41:05 -0800

    type.py

--- app/routes/type.py
+++ app/routes/type.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import Optional
 from database.db import get_db
 from models.type import Type
 from sqlalchemy import or_
 
 router = APIRouter(prefix="/type", tags=["Type"])
-
-
 
 
 
@@ -46,21 +45,29 @@ def create_type(
 
 @router.get("/")
 def get_types(db: Session = Depends(get_db)):
-    return db.query(Type).filter(
+    types = db.query(Type).filter(
         or_(
             Type.is_active == True,
             Type.is_active == None
         )
     ).all()
 
+   
+    if not types:
+        raise HTTPException(
+            status_code=404,
+            detail="No data found"
+        )
+
+    return types
 
 
 
 @router.put("/{type_id}")
 def update_type(
     type_id: int,
-    name: str,
-    is_active: bool,
+    name: Optional[str] = None,
+    is_active: Optional[bool] = None,
     db: Session = Depends(get_db)
 ):
     type_obj = db.query(Type).filter(Type.id == type_id).first()
@@ -68,20 +75,31 @@ def update_type(
     if not type_obj:
         raise HTTPException(status_code=404, detail="Type not found")
 
-    # IMPORTANT CHECK
-    existing = db.query(Type).filter(
-        Type.name == name.upper(),
-        Type.id != type_id
-    ).first()
+    # ✅ If name provided → check duplicate
+    if name is not None:
+        existing = db.query(Type).filter(
+            Type.name == name.upper(),
+            Type.id != type_id
+        ).first()
 
-    if existing:
+        if existing:
+            raise HTTPException(
+                status_code=400,
+                detail="Type name already exists"
+            )
+
+        type_obj.name = name.upper()
+
+    # ✅ If is_active provided → update
+    if is_active is not None:
+        type_obj.is_active = is_active
+
+    # ❗ Optional: If nothing provided
+    if name is None and is_active is None:
         raise HTTPException(
             status_code=400,
-            detail="Type name already exists"
+            detail="At least one field (name or is_active) must be provided"
         )
-
-    type_obj.name = name.upper()
-    type_obj.is_active = is_active
 
     db.commit()
     db.refresh(type_obj)
@@ -92,8 +110,6 @@ def update_type(
         "name": type_obj.name,
         "is_active": type_obj.is_active
     }
-
-
 
 
 
