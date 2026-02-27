50cca252502d4671f83818725174f10924d028dc
Preetha <palanisamypreetha500@gmail.com>
2026-02-27 12:35:25 -0800

    shop.py

--- app/database/shop.py
+++ app/database/shop.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class ShopCreate(BaseModel):
     shop_name: str
@@ -11,10 +12,13 @@ class ShopCreate(BaseModel):
 
 
 class ShopUpdate(BaseModel):
-    shop_name: str | None = None
-    address: str | None = None
-    certificate_no: str | None = None
-    gst_number: str | None = None
-    bank_name: str | None = None
-    account_number: str | None = None
-    IFSC_Code: str | None = None
+    shop_name: Optional[str] = None
+    address: Optional[str] = None
+    certificate_no: Optional[str] = None
+    gst_number: Optional[str] = None
+    bank_name: Optional[str] = None
+    account_number: Optional[str] = None
+    IFSC_Code: Optional[str] = None
+
+    class Config:
+        from_attributes = True
