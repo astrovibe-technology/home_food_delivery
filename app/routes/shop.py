7159cd2fb2e842c7f92b25924abb0ac4c5025959
Preetha <palanisamypreetha500@gmail.com>
2026-02-27 12:39:59 -0800

    shop.py

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from typing import Optional
 from database.db import get_db
 from models.shop import Shop
 from database.shop import ShopCreate, ShopUpdate
@@ -40,22 +40,62 @@ def get_shop(owner_id: int, db: Session = Depends(get_db)):
 
 # ------------------------------------------UPDATE-----------------------------
 
-@router.put("/update/{owner_id}")
-def update_shop(
+@router.put("/{owner_id}")
+def update_shop_profile(
     owner_id: int,
-    payload: ShopUpdate,
+    shop_name: Optional[str] = None,
+    address: Optional[str] = None,
+    certificate_no: Optional[str] = None,
+    gst_number: Optional[str] = None,
+    bank_name: Optional[str] = None,
+    account_number: Optional[str] = None,
+    IFSC_Code: Optional[str] = None,
     db: Session = Depends(get_db)
 ):
     shop = db.query(Shop).filter(Shop.owner_id == owner_id).first()
-    if not shop:
-        raise HTTPException(404, "Shop profile not found")
 
-    for key, value in payload.dict(exclude_unset=True).items():
-        setattr(shop, key, value)
+    if not shop:
+        raise HTTPException(status_code=404, detail="Shop not found")
+
+   
+    if shop_name is not None:
+        shop.shop_name = shop_name
+
+    if address is not None:
+        shop.address = address
+
+    if certificate_no is not None:
+        shop.certificate_no = certificate_no
+
+    if gst_number is not None:
+        shop.gst_number = gst_number
+
+    if bank_name is not None:
+        shop.bank_name = bank_name
+
+    if account_number is not None:
+        shop.account_number = account_number
+
+    if IFSC_Code is not None:
+        shop.IFSC_Code = IFSC_Code.upper()
 
     db.commit()
+    db.refresh(shop)
 
-    return {"message": "Shop profile updated"}
+    return {
+        "message": "Shop profile updated successfully",
+        "data": {
+            "id": shop.id,
+            "owner_id": shop.owner_id,
+            "shop_name": shop.shop_name,
+            "address": shop.address,
+            "certificate_no": shop.certificate_no,
+            "gst_number": shop.gst_number,
+            "bank_name": shop.bank_name,
+            "account_number": shop.account_number,
+            "IFSC_Code": shop.IFSC_Code
+        }
+    }
 
 
 
